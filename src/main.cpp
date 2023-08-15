0d20b7f4c0d6855ffded616d7d246ce4c8049e5c
silkyspider <james.hamilton.2001@outlook.com>
2023-08-15 18:47:56 +0100

    ui fundementals, needs a lil tidy tho

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,8 @@
 #include <rlgl.h>
 #include <raymath.h>
 #include <iostream>
+#include <cstdlib>
+#include <regex>
 
 #define RAYGUI_IMPLEMENTATION
 #include "raygui.h"
@@ -21,14 +23,133 @@ int bound;
 Camera2D camera;
 ParticleLife particleLife;
 
+Vector2 mouseScreenPosition;
+Vector2 mouseWorldPosition;
+float mouseWheelMovement;
+
+bool* activeElementBoolPtr;
 bool gridActive;
+bool stepTextBoxActive;    char stepText[6];
+
+Rectangle sidebarBounds;
+Rectangle gridCheckBounds;
+
+Rectangle stepLabelBounds, stepTextBoxBounds;
 
 
 void init();
 void update();
 void render();
 
+bool textIsFloat(char text[6]);
+bool selectTextBox(Rectangle bounds, bool* isActivePtr);
+bool submitTextBoxFloat(char text[6], float* valuePtr);
+void ui();
 
+
+int main()
+{
+    init();
+
+    while (!WindowShouldClose())
+        update(), render();
+
+    CloseWindow();
+    return EXIT_SUCCESS;
+}
+
+void update()
+{
+    // get mouse input
+    mouseScreenPosition = GetMousePosition();
+    mouseWorldPosition = GetScreenToWorld2D(mouseScreenPosition, camera);
+    mouseWheelMovement = GetMouseWheelMove();
+
+    // step over simulation
+    particleLife.update();
+}
+
+bool textIsFloat(char text[6])
+{
+    std::regex floatPattern("^[-+]?[0-9]*\\.?[0-9]+([eE][-+]?[0-9]+)?$");
+    return std::regex_match(text, floatPattern);
+}
+
+bool selectTextBox(Rectangle bounds, bool* isActivePtr)
+{
+    bool activated = CheckCollisionPointRec(mouseScreenPosition, bounds);
+    if (activated) {
+        if (activeElementBoolPtr != nullptr)
+            *activeElementBoolPtr = false;
+        *isActivePtr = true;
+        activeElementBoolPtr = isActivePtr;
+    }
+    return activated;
+}
+
+bool submitTextBoxFloat(char text[6], float* valuePtr)
+{
+    return true;
+}
+
+void ui()
+{
+    // handle camera pan on right click + drag
+    if (IsMouseButtonDown(MOUSE_BUTTON_RIGHT)) {
+        Vector2 scaledInvMouseDelta = Vector2Scale(GetMouseDelta(), -1.0f / camera.zoom);
+        camera.target = Vector2Add(camera.target, scaledInvMouseDelta);
+    }
+
+    // handle camera zoom on scroll
+    if (mouseWheelMovement != 0) {
+        camera.target = GetScreenToWorld2D(camera.offset, camera);
+        camera.zoom += (mouseWheelMovement * 2.0f);
+        if (camera.zoom < 2.0f) camera.zoom = 2.0f;
+    }
+
+    // sidebar option selection
+    if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
+        if (selectTextBox(stepTextBoxBounds, &stepTextBoxActive));
+        else if (activeElementBoolPtr != nullptr)
+            *activeElementBoolPtr = false,
+            activeElementBoolPtr = nullptr;
+    }
+
+    // handle textbox submission
+    if ((IsKeyPressed(KEY_ENTER) || IsKeyPressed(KEY_KP_ENTER))) {
+        if (activeElementBoolPtr != nullptr) {
+
+        }
+    }
+
+    DrawFPS(windowWidth - 80, 5);
+
+    DrawRectangleRec(sidebarBounds, LIGHTGRAY);
+
+    GuiCheckBox(gridCheckBounds, "Draw Grid", &gridActive);
+
+
+    GuiDrawText("Step", stepLabelBounds, TEXT_ALIGN_RIGHT, textIsFloat(stepText) ? DARKGRAY : RED);
+    GuiTextBox(stepTextBoxBounds, stepText, 6, stepTextBoxActive);
+}
+
+void render()
+{
+    BeginDrawing();
+        ClearBackground(BLACK);
+
+        BeginMode2D(camera);
+            if (gridActive)
+                for (int i = -bound; i <= bound; i++)
+                    DrawLine(2.0f*i, 2.0f*-bound, 2.0f*i, 2.0f*bound, DARKGRAY),
+                    DrawLine(2.0f*-bound, 2.0f*i, 2.0f*bound, 2.0f*i, DARKGRAY);
+            particleLife.draw();
+        EndMode2D();
+
+        ui();
+
+    EndDrawing();
+}
 
 void init()
 {
@@ -45,64 +166,20 @@ void init()
     camera.rotation = 0.0f;
     camera.zoom = 1.0f;
 
-    // scene
-    gridActive = true;
-
     // simulation
     count = 1024;
     size = 16;
     bound = size/2;
     particleLife.init(count, size);
-}
 
-void update()
-{
-    // pan and zoom controls
-    if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
-        Vector2 scaledInvMouseDelta = Vector2Scale(GetMouseDelta(), -1.0f / camera.zoom);
-        camera.target = Vector2Add(camera.target, scaledInvMouseDelta);
-    }
-    float wheel = GetMouseWheelMove();
-    if (wheel != 0) {
-        camera.target = GetScreenToWorld2D(camera.offset, camera);
-        camera.zoom += (wheel * 2.0f);
-        if (camera.zoom < 2.0f)
-            camera.zoom = 2.0f;
-    }
+    // ui active option
+    activeElementBoolPtr = nullptr;
+    gridActive = false;
+    stepTextBoxActive = false;
 
-    // step over simulation
-    particleLife.update();
-}
-
-void render()
-{
-    BeginDrawing();
-        ClearBackground(DARKGRAY);
-
-        // grid and particles
-        BeginMode2D(camera);
-            if (gridActive)
-                for (int i = -bound; i <= bound; i++)
-                    DrawLine(2.0f*i, 2.0f*-bound, 2.0f*i, 2.0f*bound, GRAY),
-                    DrawLine(2.0f*-bound, 2.0f*i, 2.0f*bound, 2.0f*i, GRAY);
-            particleLife.draw();
-        EndMode2D();
-
-        // user interface
-        DrawFPS(windowWidth - 80, 5);
-
-    EndDrawing();
-}
-
-
-
-int main()
-{
-    init();
-
-    while (!WindowShouldClose())
-        update(), render();
-
-    CloseWindow();
-    return EXIT_SUCCESS;
+    // ui bounds
+    sidebarBounds = { 0, 0, 200, (float)(windowHeight) };
+    gridCheckBounds = { 80, 80, 20, 20 };
+    stepTextBoxBounds = { 80, 100, 100, 20 };
+    stepLabelBounds = { 0, 100, 80, 20 };
 }
